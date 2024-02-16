285c90d954e7308415be0dfc3bcd5fcdfb4803a2
PapaYawAsiamah <99272425+PapaYawAsiamah@users.noreply.github.com>
2024-02-16 09:03:56 +0000

    made a few corrections

--- assignment.py
+++ assignment.py
@@ -1,9 +1,11 @@
 currrent_users = ['kofi', 'Ama', 'Kwame', 'Kwabena', 'Esi', 'Joe']
-new_users = ['kofi', 'George', 'Esi', 'Ekua']
+new_users = ['kofi', 'George', 'esi', 'Ekua']
+
+current_users_lower = [user.lower() for user in currrent_users]
 
 
 for new_user in new_users:
-    if new_user in currrent_users:
+    if new_user.lower() in current_users_lower:
         print(f"{new_user} already exists")
     else :
         print(f"{new_user} is available")
